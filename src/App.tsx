1438df3d3fee1c1b40f2d8d8437f2219a1b94afd
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-04-05 19:44:05 +0100

    Base Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, Container, Stack} from "react-bootstrap";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={"my-4"}>
+      <Stack direction={"horizontal"} className={"mb-4"} gap={2}>
+        <h1 className={"me-auto"}>Budgets</h1>
+        <Button variant={"primary"}>Add Budget</Button>
+        <Button variant={"outline-primary"}>Add Expense</Button>
+      </Stack>
+    </Container>
   );
 }
 
